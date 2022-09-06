8f05d90c7186c93133dc67358a0f6f08544bc9e3
myqdh <badrusalsyihab@gmail.com>
2022-09-06 10:29:40 +0700

    add change to public

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static('build'))
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: path.join(__dirname, 'build')});
+    res.sendFile('index.html', {root: path.join(__dirname, 'public')});
 })
 
 app.listen(process.env.PORT || 3000);
